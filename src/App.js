7b5dd876769d5077ca8f432562f4b8c6de48ba91
MarsiaRochelin <marsrochelin@pursuit.org>
2023-09-06 16:02:48 -0400

    useEffect func & slices of State created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import SearchBar from './components/SearchBar/SearchBar';
-import Users from './components/Users/Users';
-import './App.css';
+import SearchBar from "./components/SearchBar/SearchBar";
+import Users from "./components/Users/Users";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const API_URL = "https://users-app-backend.onrender.com";
 
 function App() {
-  // TODO: Fetch data here
+  const [usersData, setUsersData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
+  // TODO: Fetch data here
+  useEffect(() => {
+    // console.log("<App /> useEffect() fired");
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const response = await fetch(`${API_URL}/users`);
+        // console.log(response);
+        const json = await response.json();
+        // console.log("<App /> useEffect() feteched data", json);
+        const { data, error } = json;
+        if (response.ok) {
+          setUsersData(data);
+          setLoading(false);
+          //create and error to see state update
+        } else {
+          setError(error);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(`App /> useEffect error: ${err.message}`);
+        setLoading(false);
+        setError(err.message);
+      }
+    }
+    fetchData();
+  }, []);
+  console.log(usersData);
   return (
     <div className="App">
       <h1>Our Users</h1>
